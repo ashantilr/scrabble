a01e8c275ec46ad6f6e1ea031dfdfbaef572d985
Ashanti <106094234+ashantilr@users.noreply.github.com>
2024-12-09 15:34:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,10 +14,7 @@ window.addEventListener("DOMContentLoaded", () => {
   game.render(boardGridElement);
 });
 
-// We check to make sure the play button exists before adding the event
-// listener. You are to add the play button as part of this homework. If we do
-// not test if it exists, the code will break when running the tests. You can
-// safely remove this conditional after you add the play button.
+
 if (playButtonElement) {
   playButtonElement.addEventListener("click", () => {
     
